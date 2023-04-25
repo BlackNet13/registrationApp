11fbb558a59e01a2ab80ddca01af8e67d6033c64
BlackNet13 <leo7slashsides@gmail.com>
2023-04-25 18:52:04 +0800

    Initial commit

---
+++ app/src/main/java/sg/rp/edu/rp/c346/id22038845/registrationApp/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.rp.edu.rp.c346.id22038845.registrationApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
